6ac479691a10cc5714ca5e2f70e66b10760194f8
Thatgfsj <thatgfsj@gmail.com>
2026-06-24 22:12:11 +0800

    Fix scrolling - recreate visible buttons only

--- vibe-copy.cpp
+++ vibe-copy.cpp
@@ -12,16 +12,18 @@ struct Snippet {
     std::wstring actualText;
 };
 
+// 全局变量
+HINSTANCE g_hInst = NULL;
 std::vector<Snippet> g_snippets;
+HWND g_hMainWnd = NULL;
+HWND g_hScrollWnd = NULL;
 HWND g_hStatus = NULL;
 HWND g_hEdit = NULL;
 HWND g_hReload = NULL;
 HWND g_hTop = NULL;
-HWND g_hScrollArea = NULL;
 HFONT g_hFont = NULL;
 bool g_isTopmost = true;
-int g_scrollY = 0;
-int g_totalHeight = 0;
+int g_scrollPos = 0;
 
 std::wstring Utf8ToWide(const std::string& utf8) {
     if (utf8.empty()) return L"";
@@ -118,53 +120,63 @@ void LoadSnippets() {
     file.close();
 }
 
-void UpdateScrollBar(HWND hwnd) {
+int GetTotalHeight() {
+    int rows = ((int)g_snippets.size() + 1) / 2;
+    return 10 + rows * 60;
+}
+
+void UpdateScrollInfo() {
     RECT rc;
-    GetClientRect(g_hScrollArea, &rc);
+    GetClientRect(g_hScrollWnd, &rc);
     int visibleHeight = rc.bottom;
 
     SCROLLINFO si = {0};
     si.cbSize = sizeof(SCROLLINFO);
     si.fMask = SIF_RANGE | SIF_PAGE | SIF_POS;
     si.nMin = 0;
-    si.nMax = g_totalHeight;
+    si.nMax = GetTotalHeight();
     si.nPage = visibleHeight;
-    si.nPos = g_scrollY;
-    SetScrollInfo(g_hScrollArea, SB_VERT, &si, TRUE);
+    si.nPos = g_scrollPos;
+    SetScrollInfo(g_hScrollWnd, SB_VERT, &si, TRUE);
 }
 
-void CreateButtons(HWND hwnd) {
-    // 清除滚动区域内的子窗口
-    HWND child = GetWindow(g_hScrollArea, GW_CHILD);
+void RecreateButtons() {
+    // 删除所有按钮
+    HWND child = GetWindow(g_hScrollWnd, GW_CHILD);
     while (child) {
         HWND next = GetWindow(child, GW_HWNDNEXT);
         DestroyWindow(child);
         child = next;
     }
 
+    // 如果没有数据就返回
+    if (g_snippets.empty()) return;
+
     RECT rc;
-    GetClientRect(g_hScrollArea, &rc);
-    int tableWidth = rc.right - 10;
-    int btnWidth = (tableWidth - 5) / 2;
+    GetClientRect(g_hScrollWnd, &rc);
+    int areaWidth = rc.right;
+    int btnWidth = (areaWidth - 15) / 2;
     int btnHeight = 50;
     int col = 0, row = 0;
 
     for (size_t i = 0; i < g_snippets.size(); i++) {
         int x = 5 + col * (btnWidth + 5);
-        int y = 5 + row * (btnHeight + 5) - g_scrollY;
+        int y = 5 + row * (btnHeight + 5) - g_scrollPos;
 
-        HWND hBtn = CreateWindowW(L"BUTTON", g_snippets[i].displayName.c_str(),
-            WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON | BS_MULTILINE,
-            x, y, btnWidth, btnHeight,
-            g_hScrollArea, (HMENU)(INT_PTR)(1000 + i), NULL, NULL);
-        SendMessage(hBtn, WM_SETFONT, (WPARAM)g_hFont, TRUE);
+        // 只创建可见区域的按钮
+        if (y + btnHeight > 0 && y < rc.bottom) {
+            HWND hBtn = CreateWindowW(L"BUTTON", g_snippets[i].displayName.c_str(),
+                WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON | BS_MULTILINE,
+                x, y, btnWidth, btnHeight,
+                g_hScrollWnd, (HMENU)(INT_PTR)(1000 + i), g_hInst, NULL);
+            SendMessage(hBtn, WM_SETFONT, (WPARAM)g_hFont, TRUE);
+        }
 
         col++;
         if (col >= 2) { col = 0; row++; }
     }
 
-    g_totalHeight = 5 + (row + (col > 0 ? 1 : 0)) * (btnHeight + 5);
-    UpdateScrollBar(hwnd);
+    UpdateScrollInfo();
 }
 
 void CopyToClipboard(const std::wstring& text) {
@@ -181,12 +193,13 @@ void CopyToClipboard(const std::wstring& text) {
     CloseClipboard();
 }
 
-LRESULT CALLBACK ScrollAreaProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
+LRESULT CALLBACK ScrollWndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
     switch (msg) {
         case WM_VSCROLL: {
             RECT rc;
             GetClientRect(hwnd, &rc);
             int visibleHeight = rc.bottom;
+            int totalHeight = GetTotalHeight();
 
             SCROLLINFO si = {0};
             si.cbSize = sizeof(SCROLLINFO);
@@ -195,27 +208,17 @@ LRESULT CALLBACK ScrollAreaProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lPara
 
             int oldPos = si.nPos;
             switch (LOWORD(wParam)) {
-                case SB_LINEUP: si.nPos -= 30; break;
-                case SB_LINEDOWN: si.nPos += 30; break;
+                case SB_LINEUP: si.nPos -= 60; break;
+                case SB_LINEDOWN: si.nPos += 60; break;
                 case SB_PAGEUP: si.nPos -= visibleHeight; break;
                 case SB_PAGEDOWN: si.nPos += visibleHeight; break;
                 case SB_THUMBTRACK: si.nPos = si.nTrackPos; break;
             }
 
-            si.nPos = (std::max)(0, (std::min)(si.nPos, g_totalHeight - visibleHeight));
+            si.nPos = (std::max)(0, (std::min)(si.nPos, totalHeight - visibleHeight));
             if (si.nPos != oldPos) {
-                g_scrollY = si.nPos;
-                SetScrollPos(hwnd, SB_VERT, si.nPos, TRUE);
-                // 移动所有子窗口
-                int dy = oldPos - si.nPos;
-                HWND child = GetWindow(hwnd, GW_CHILD);
-                while (child) {
-                    RECT btnRc;
-                    GetWindowRect(child, &btnRc);
-                    MapWindowPoints(GetParent(child), hwnd, (LPPOINT)&btnRc, 2);
-                    MoveWindow(child, btnRc.left, btnRc.top + dy, btnRc.right - btnRc.left, btnRc.bottom - btnRc.top, TRUE);
-                    child = GetWindow(child, GW_HWNDNEXT);
-                }
+                g_scrollPos = si.nPos;
+                RecreateButtons();
             }
             return 0;
         }
@@ -229,39 +232,44 @@ LRESULT CALLBACK ScrollAreaProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lPara
     return DefWindowProcW(hwnd, msg, wParam, lParam);
 }
 
-LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
+LRESULT CALLBACK MainWndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
     switch (msg) {
         case WM_CREATE: {
             g_hFont = CreateFontW(-16, 0, 0, 0, FW_NORMAL, FALSE, FALSE, FALSE,
                 DEFAULT_CHARSET, OUT_DEFAULT_PRECIS, CLIP_DEFAULT_PRECIS,
                 CLEARTYPE_QUALITY, DEFAULT_PITCH | FF_SWISS, L"Segoe UI");
 
-            // 注册滚动区域窗口类
+            // 注册滚动窗口类
             WNDCLASSEXW wc = {0};
             wc.cbSize = sizeof(WNDCLASSEXW);
-            wc.lpfnWndProc = ScrollAreaProc;
-            wc.hInstance = GetModuleHandle(NULL);
+            wc.lpfnWndProc = ScrollWndProc;
+            wc.hInstance = g_hInst;
             wc.hCursor = LoadCursor(NULL, IDC_ARROW);
             wc.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
-            wc.lpszClassName = L"VibeCopyScrollArea";
+            wc.lpszClassName = L"VibeCopyScroll";
             RegisterClassExW(&wc);
 
-            // 创建滚动区域
-            g_hScrollArea = CreateWindowExW(0, L"VibeCopyScrollArea", L"",
+            // 创建滚动窗口
+            RECT rc;
+            GetClientRect(hwnd, &rc);
+            g_hScrollWnd = CreateWindowExW(0, L"VibeCopyScroll", L"",
                 WS_CHILD | WS_VISIBLE | WS_VSCROLL | WS_CLIPCHILDREN,
-                10, 10, 780, 175, hwnd, NULL, NULL, NULL);
+                10, 10, rc.right - 20, rc.bottom - 110,
+                hwnd, NULL, g_hInst, NULL);
 
             // 底部按钮
+            int bottomY = rc.bottom - 90;
             g_hEdit = CreateWindowW(L"BUTTON", L"编辑配置", WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,
-                10, 195, 250, 50, hwnd, (HMENU)1, NULL, NULL);
+                10, bottomY, 250, 50, hwnd, (HMENU)1, g_hInst, NULL);
             g_hReload = CreateWindowW(L"BUTTON", L"刷新列表", WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,
-                275, 195, 250, 50, hwnd, (HMENU)2, NULL, NULL);
+                275, bottomY, 250, 50, hwnd, (HMENU)2, g_hInst, NULL);
             g_hTop = CreateWindowW(L"BUTTON", L"取消置顶", WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,
-                540, 195, 250, 50, hwnd, (HMENU)3, NULL, NULL);
+                540, bottomY, 250, 50, hwnd, (HMENU)3, g_hInst, NULL);
 
             // 状态栏
             g_hStatus = CreateWindowW(L"STATIC", L"准备就绪... 单击大色块即可复制",
-                WS_CHILD | WS_VISIBLE | SS_LEFT, 10, 255, 780, 35, hwnd, NULL, NULL, NULL);
+                WS_CHILD | WS_VISIBLE | SS_LEFT,
+                10, rc.bottom - 35, rc.right - 20, 35, hwnd, NULL, g_hInst, NULL);
 
             SendMessage(g_hEdit, WM_SETFONT, (WPARAM)g_hFont, TRUE);
             SendMessage(g_hReload, WM_SETFONT, (WPARAM)g_hFont, TRUE);
@@ -269,7 +277,7 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
             SendMessage(g_hStatus, WM_SETFONT, (WPARAM)g_hFont, TRUE);
 
             LoadSnippets();
-            CreateButtons(hwnd);
+            RecreateButtons();
             break;
         }
 
@@ -280,9 +288,10 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
                 ShellExecuteW(NULL, L"open", L"notepad.exe", path.c_str(), NULL, SW_SHOW);
             }
             else if (id == 2) {
-                g_scrollY = 0;
+                g_scrollPos = 0;
                 LoadSnippets();
-                CreateButtons(hwnd);
+                RecreateButtons();
+                SetWindowTextW(g_hStatus, L"已刷新列表");
             }
             else if (id == 3) {
                 g_isTopmost = !g_isTopmost;
@@ -305,7 +314,7 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
             GetClientRect(hwnd, &rc);
             int scrollHeight = rc.bottom - 110;
 
-            MoveWindow(g_hScrollArea, 10, 10, rc.right - 20, scrollHeight, TRUE);
+            MoveWindow(g_hScrollWnd, 10, 10, rc.right - 20, scrollHeight, TRUE);
 
             int bottomY = rc.bottom - 90;
             MoveWindow(g_hEdit, 10, bottomY, 250, 50, TRUE);
@@ -313,8 +322,8 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
             MoveWindow(g_hTop, 540, bottomY, 250, 50, TRUE);
             MoveWindow(g_hStatus, 10, rc.bottom - 35, rc.right - 20, 35, TRUE);
 
-            g_scrollY = 0;
-            CreateButtons(hwnd);
+            g_scrollPos = 0;
+            RecreateButtons();
             break;
         }
 
@@ -330,24 +339,26 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
 }
 
 int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, LPWSTR, int nCmdShow) {
+    g_hInst = hInstance;
+
     WNDCLASSEXW wc = {0};
     wc.cbSize = sizeof(WNDCLASSEXW);
     wc.style = CS_HREDRAW | CS_VREDRAW;
-    wc.lpfnWndProc = WndProc;
+    wc.lpfnWndProc = MainWndProc;
     wc.hInstance = hInstance;
     wc.hCursor = LoadCursor(NULL, IDC_ARROW);
     wc.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
     wc.lpszClassName = L"VibeCopyClass";
     RegisterClassExW(&wc);
 
-    HWND hwnd = CreateWindowExW(WS_EX_TOPMOST, L"VibeCopyClass", L"VibeCopy 鼠标速复",
+    g_hMainWnd = CreateWindowExW(WS_EX_TOPMOST, L"VibeCopyClass", L"VibeCopy 鼠标速复",
         WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, 800, 300,
         NULL, NULL, hInstance, NULL);
 
-    if (!hwnd) return 1;
+    if (!g_hMainWnd) return 1;
 
-    ShowWindow(hwnd, nCmdShow);
-    UpdateWindow(hwnd);
+    ShowWindow(g_hMainWnd, nCmdShow);
+    UpdateWindow(g_hMainWnd);
 
     MSG msg;
     while (GetMessage(&msg, NULL, 0, 0)) {
